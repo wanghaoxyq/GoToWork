71d655b96ca46ebfb9dd64fa644ab95a5d9ff81c
kingwhite <503334261@qq.com>
2015-05-29 20:50:55 +0800

    Project initialize.

---
+++ js/blocks.js
@@ -0,0 +1,44 @@
+function getDomainFromUrl(url){
+	var host = "null";
+	if(typeof url == "undefined" || url == null)
+		url = window.location.href;
+	var regex = /.*\:\/\/([^\/]*).*/;
+	var match = url.match(regex);
+	if(typeof match != "undefined" && match != null)
+		host = match[1];
+	return host;
+}
+
+function checkForUrl(tabId, info, tab){
+	if(info.status == 'loading'){
+		var domain = getDomainFromUrl(tab.url).toLowerCase();
+		switch(domain){
+			case "weibo.com":
+			case "www.zhihu.com":
+			case "www.douban.com":
+			case "qzone.qq.com":
+			case "user.qzone.qq.com":
+			case "www.bilibili.com":
+			case "www.acfun.com":
+			case "www.4399.com":
+			case "www.renren.com":
+			case "www.iqiyi.com":
+			case "www.tudou.com":
+			case "www.youku.com":
+			case "www.pps.tv":
+			case "www.pptv.com":
+			case "tv.sohu.com":
+			case "www.letv.com":
+			case "www.56.com":
+			case "www.ku6.com":
+			case "tieba.baidu.com":
+				chrome.tabs.update({
+					'url' : "GoToWork.html"
+				});
+				break;
+			default: break;
+		}
+	}
+};
+
+chrome.tabs.onUpdated.addListener(checkForUrl);
